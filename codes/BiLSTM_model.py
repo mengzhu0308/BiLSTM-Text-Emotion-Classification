a4788007a44e2e00c354f8c2bce3488ec381f93a
Meng <75422140+mengzhu0308@users.noreply.github.com>
2021-01-04 13:31:50 +0800

    Update BiLSTM_model.py
    不能使用ReLU激活函数，否则损失值容易爆炸

--- codes/BiLSTM_model.py
+++ codes/BiLSTM_model.py
@@ -12,10 +12,10 @@ from MyBidirectional import MyBidirectional
 
 def BiLSTM_Model(x, vocab_size, hidden_dim, num_classes=4):
     x = Embedding(vocab_size, hidden_dim, mask_zero=True)(x)
-    x = MyBidirectional(LSTM(hidden_dim, return_sequences=True, activation='relu'))(x)
-    x = MyBidirectional(LSTM(hidden_dim * 2, return_sequences=True, activation='relu'))(x)
-    x = MyBidirectional(LSTM(hidden_dim * 4, return_sequences=True, activation='relu'))(x)
-    x = MyBidirectional(LSTM(hidden_dim * 8, activation='relu'))(x)
+    x = MyBidirectional(LSTM(hidden_dim, return_sequences=True))(x)
+    x = MyBidirectional(LSTM(hidden_dim * 2, return_sequences=True))(x)
+    x = MyBidirectional(LSTM(hidden_dim * 4, return_sequences=True))(x)
+    x = MyBidirectional(LSTM(hidden_dim * 8))(x)
     x = Dense(num_classes)(x)
 
     return x
